6cb1e4e21a44a3d20d5b16739a42a8ad8d0a8694
hbc-desktop <1242582816@qq.com>
2022-01-17 17:25:52 +0800

    commit from idea

--- javaCore1/C4/src/javaDocPrac.java
+++ javaCore1/C4/src/javaDocPrac.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
+/**s
+ * @author hbcccc
+ * <img src="doc-files/pic.png" />
+ */
 public class javaDocPrac {
+    private String name = "hbcccc";
+
+    /**
+     * this method does nothing!
+     * <img src="../doc-files/pic.png" />
+     */
+    public void foo(){
+        System.out.println("test!");
+    }
+
+    // run: javadoc -d ../docDirectory *.java
+    //notice: must be in /src as root dir
+    public static void main(String[] args){
+        javaDocPrac j = new javaDocPrac();
+        j.foo();
+    }
 }
+
+//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
