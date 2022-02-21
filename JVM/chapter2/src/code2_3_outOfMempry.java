90ea7a7a2fd43bce27cd0c7b32316298e66b10e6
hbcccc <1242582816@qq.com>
2022-02-21 23:01:23 +0800

    some update of code_2_3

--- JVM/chapter2/src/code2_3_outOfMempry.java
+++ JVM/chapter2/src/code2_3_outOfMempry.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+* VM options: -Xms20m +Xmx20m
+* The objects will be stored in java heap. when this space is full, it will throw an Error
+* */
 public class code2_3_outOfMempry {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 //        System.out.println("Hello, world!");
         List<code2_3_outOfMempry> list = new ArrayList<>();
-        while(true){
+        while (true) {
             list.add(new code2_3_outOfMempry());
         }
     }
